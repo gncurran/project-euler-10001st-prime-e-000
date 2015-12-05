7f660f5fe2a095097b8e03acc914941977913b80
gncurran <gnc115@gmail.com>
2015-12-04 22:01:27 -0500

    Done.

--- lib/10001st_prime.rb
+++ lib/10001st_prime.rb
@@ -1,1 +1,13 @@
-# Implement your procedural solution here!
+# Find the 10001st prime number
+require 'prime'
+
+def prime_number_for(n)
+  i = 0
+  find_prime = 1
+  loop do
+    i += 1 if find_prime.prime?
+    break if i == n
+    find_prime += 1
+  end
+  return find_prime
+end
--- lib/oo_10001st_prime.rb
+++ lib/oo_10001st_prime.rb
@@ -1,1 +1,19 @@
-# Implement your object-oriented solution here!
+# Find the 10001st prime number
+class Prime
+  attr_accessor :n
+
+  def initialize(n)
+    @n = n
+  end
+
+  def number
+    i = 0
+    find_prime = 1
+    loop do
+      i += 1 if find_prime.prime?
+      break if i == n
+      find_prime += 1
+  end
+  return find_prime
+  end
+end
